196a19eb8654a6923f847774abe3ce15be788fc6
HyoJeong Moon <57999419+kot8585@users.noreply.github.com>
2023-04-28 19:19:51 +0900

    react native storybook 설정된거 다운받음

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native';
+import StorybookUIRoot from './.ondevice/Storybook';
+export {StorybookUIRoot as default};
+
+const App = () => {
+  return (
+    <View>
+      <Text>hello</Text>
+    </View>
+  );
+};
+
+// export default App;
